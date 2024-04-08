ea55fccc9bfb1d9d4159e530ae8c81422227d049
Ravi Maurya <lcs2020043@iiitl.ac.in>
2024-04-09 01:54:40 +0530

    create list and create inventory view

--- inventory/views.py
+++ inventory/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from rest_framework import generics
+from inventory.models import Inventory
+from inventory.serializers import InventorySerializer
 
 # Create your views here.
 
 
+class ListCreateInventoryView(generics.ListCreateAPIView):
+    queryset = Inventory.objects.all()
+    serializer_class = InventorySerializer
